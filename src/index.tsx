6c5c81c27aba5b38b128c2b3aefaa0a5c9f80d20
alirezafz79 <alirezafazel379@gmail.com>
2022-08-02 01:13:55 +0430

    updated main index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import RequireAuth from './components/RequireAuth';
+import Home from './pages/home';
+import Login from './pages/login';
+import Admin from './pages/admin';
 import reportWebVitals from './reportWebVitals';
+import routes from './routes';
+import AuthProvider from './context/AuthProvider';
+import './scss/globals.scss';
+import './scss/iransans-font.scss';
+import 'react-toastify/dist/ReactToastify.min.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<BrowserRouter>
+		<AuthProvider>
+			<div className='container'>
+				<ToastContainer />
+				<Routes>
+					<Route
+						path={routes.home}
+						element={
+							<RequireAuth>
+								<Home />
+							</RequireAuth>
+						}
+					/>
+					<Route path={routes.login} element={<Login />} />
+					<Route
+						path={routes.admin}
+						element={
+							<RequireAuth>
+								<Admin />
+							</RequireAuth>
+						}
+					/>
+				</Routes>
+			</div>
+		</AuthProvider>
+	</BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
