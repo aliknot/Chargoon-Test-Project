80a4381c32acd375eb7b408eb1d19dea9325a538
alirezafz79 <alirezafazel379@gmail.com>
2022-08-01 16:30:10 +0430

    removed redundant imports in firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,19 +1,13 @@
-import { initializeApp } from 'firebase/app'
-import {
-  getFirestore, collection, onSnapshot,
-  addDoc, deleteDoc, doc,
-  query, where,
-  orderBy, serverTimestamp,
-  updateDoc
-} from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, doc } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCs1cpCd3-uPko0PnM-Bf4Mcm1ZXczUA4Q",
-  authDomain: "employmentproject-302bd.firebaseapp.com",
-  projectId: "employmentproject-302bd",
-  storageBucket: "employmentproject-302bd.appspot.com",
-  messagingSenderId: "1005370091445",
-  appId: "1:1005370091445:web:8ab499be2bad3f77b89bfc"
+	apiKey: 'AIzaSyCs1cpCd3-uPko0PnM-Bf4Mcm1ZXczUA4Q',
+	authDomain: 'employmentproject-302bd.firebaseapp.com',
+	projectId: 'employmentproject-302bd',
+	storageBucket: 'employmentproject-302bd.appspot.com',
+	messagingSenderId: '1005370091445',
+	appId: '1:1005370091445:web:8ab499be2bad3f77b89bfc',
 };
 
 // Initialize Firebase
@@ -21,6 +15,6 @@ initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const collectionRef = collection(db, 'Users');
 export function getDoc(id: string) {
-	const docRef = doc(db, 'Users', id)
+	const docRef = doc(db, 'Users', id);
 	return docRef;
 }
--- src/firebase/transportLayer.ts
+++ src/firebase/transportLayer.ts
@@ -1,10 +1,4 @@
-import {
-	getFirestore, collection, onSnapshot,
-	addDoc, deleteDoc, doc,
-	query, where,
-	orderBy, serverTimestamp,
-	updateDoc
-} from 'firebase/firestore';
+import { onSnapshot, addDoc, deleteDoc, query } from 'firebase/firestore';
 import { collectionRef, getDoc } from './index';
 
 export interface User {
@@ -20,7 +14,7 @@ export function getData() {
 		const q = query(collectionRef);
 		onSnapshot(q, (snapshot) => {
 			snapshot.docs.forEach((doc) => {
-				data.push({...doc.data(), id: doc.id} as User);
+				data.push({ ...doc.data(), id: doc.id } as User);
 			});
 			resolve(data);
 		});
@@ -29,18 +23,16 @@ export function getData() {
 
 export function insertData(data: User) {
 	return new Promise((resolve, reject) => {
-		addDoc(collectionRef, data)
-			.then(() => {
-				resolve(true);
-			})
+		addDoc(collectionRef, data).then(() => {
+			resolve(true);
+		});
 	});
 }
 
 export function deleteData(id: string) {
 	return new Promise((resolve, reject) => {
-		deleteDoc(getDoc(id))
-			.then(() => {
-				resolve(true);
-			})
+		deleteDoc(getDoc(id)).then(() => {
+			resolve(true);
+		});
 	});
 }
